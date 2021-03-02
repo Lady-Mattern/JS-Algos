ecb83439f97ae08fca61ea7b3375b240808a5b52
Leah Mattern <ladyleahmattern@gmail.com>
2021-03-02 15:18:49 -0500

    Two Sum exercise brute force solution

---
+++ JS-Algos.js
@@ -0,0 +1,19 @@
+// TWO SUM
+
+let arr1 = [3, 5, -4, 8, 11, 1, -1, 6];
+
+// Brute force O(n2) time | O(n) space. Worst case scenario
+
+function twoNumberSum(array, targetSum) {
+	let answer = [];
+	for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === targetSum) {
+                answer.push(array[i], array[j]);
+            } 
+        }
+    }
+    return answer;
+}
+
+console.log(twoNumberSum(arr1, 10));
